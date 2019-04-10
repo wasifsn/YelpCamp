f24f3094046c914088213a494ca93c2c52b3d297
Wasif Naquvi <wasif4000.wn@gmail.com>
2019-04-10 19:04:44 +0000

    add landing and campgrounds page

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var express = require("express");
 var app = express();
+app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
-    res.send("this will be the landing page soon!!");
+    res.render("landing");
 })
+  
+  app.get("/campgrounds", function(req,res){
+   
+    var campgrounds = [
+    {   
+        name: "Nandi hills", image: "https://imgstaticcontent.lbb.in/lbbnew/wp-content/uploads/sites/2/2018/01/21185609/NandiHills1.jpg?w=750&h=500&wm=webp&dpr=2"
+    },
     
+    {
+        name:"tipsinah", image: "http://tipsinahmoundscampground.com/wp-content/uploads/2017/07/IMG_6559-copy.jpg"
+    }
+    ]
+     res.render("campgrounds",{campgrounds: campgrounds});
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("the yelpcamp server has started");
     
